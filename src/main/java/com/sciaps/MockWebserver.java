ee53bb21679814d932013b423f57fbf2c58686a1
Stephen Gowen <sgowen@technegames.com>
2014-10-23 16:05:04 -0400

    Updating gitignore with netbeans stuff and removing unused imports in MockWebserver.java

--- src/main/java/com/sciaps/MockWebserver.java
+++ src/main/java/com/sciaps/MockWebserver.java
@@ -1,6 +1,5 @@
 package com.sciaps;
 
-
 import com.google.common.base.Throwables;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -10,7 +9,6 @@ import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.Provides;
-import com.sciaps.common.ThreadUtils;
 import com.sciaps.common.data.Standard;
 import com.sciaps.common.data.utils.StandardsLibrary;
 import org.devsmart.miniweb.Server;
@@ -19,7 +17,6 @@ import org.devsmart.miniweb.handlers.controller.Body;
 import org.devsmart.miniweb.handlers.controller.Controller;
 import org.devsmart.miniweb.handlers.controller.RequestMapping;
 import org.devsmart.miniweb.utils.RequestMethod;
-import sun.misc.IOUtils;
 
 import java.io.File;
 import java.io.FileReader;
@@ -28,32 +25,36 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
-public class MockWebserver {
-
+public final class MockWebserver
+{
     private static Injector injector;
     private static File baseDir;
     private static StandardsLibrary standardsLibrary;
 
-    static class ConfigModule extends AbstractModule {
-
-
+    static class ConfigModule extends AbstractModule
+    {
         @Override
-        protected void configure() {
-
+        protected void configure()
+        {
+            // Empty
         }
 
         @Provides
-        StandardsLibrary provideStandardsLib() throws IOException {
-            if(standardsLibrary == null) {
-                Gson gson = new GsonBuilder()
-                        .create();
+        StandardsLibrary provideStandardsLib() throws IOException
+        {
+            if (standardsLibrary == null)
+            {
+                Gson gson = new GsonBuilder().create();
 
                 File standardsLibraryFile = new File(baseDir, "assays.json");
                 JsonReader reader = new JsonReader(new FileReader(standardsLibraryFile));
-                try {
+                try
+                {
                     Standard[] standardsArray = gson.fromJson(reader, Standard[].class);
                     standardsLibrary = new StandardsLibrary(Arrays.asList(standardsArray));
-                } finally {
+                }
+                finally
+                {
                     reader.close();
                 }
             }
@@ -63,41 +64,38 @@ public class MockWebserver {
     }
 
     @Controller
-    public static class LIBZMockController {
-
+    public static class LIBZMockController
+    {
         @RequestMapping(value = "standards", method = RequestMethod.GET)
         @Body
-        public List<Standard> handleGetStandards() {
-
+        public List<Standard> handleGetStandards()
+        {
             StandardsLibrary library = injector.getInstance(StandardsLibrary.class);
 
             return library.getStandards();
-
         }
 
         @RequestMapping(value = "standards", method = RequestMethod.PUT)
-        public void handleSetStandards(@Body Standard[] standards) throws IOException {
-
+        public void handleSetStandards(@Body Standard[] standards) throws IOException
+        {
             standardsLibrary = new StandardsLibrary(Arrays.asList(standards));
 
             //save to disk
             final File standardsLibraryFile = new File(baseDir, "assays.json");
             final File tmpFile = File.createTempFile("tmp", "json", baseDir);
             JsonWriter writer = new JsonWriter(new FileWriter(tmpFile));
-            Gson gson = new GsonBuilder()
-                    .create();
+            Gson gson = new GsonBuilder().create();
 
             gson.toJson(standardsLibrary.getStandards(), Standard[].class, writer);
             writer.close();
             tmpFile.renameTo(standardsLibraryFile);
-
-
         }
     }
 
-    public static void main(String[] args) {
-        try {
-
+    public static void main(String[] args)
+    {
+        try
+        {
             baseDir = new File(args[0]);
             injector = Guice.createInjector(new ConfigModule());
 
@@ -107,8 +105,9 @@ public class MockWebserver {
                     .create();
 
             server.start();
-
-        } catch (Throwable t) {
+        }
+        catch (Throwable t)
+        {
             Throwables.propagate(t);
         }
     }

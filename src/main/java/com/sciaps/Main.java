459ef97652d7192fae94427b3759a38f1ae74d32
Stephen Gowen <sgowen@technegames.com>
2014-10-27 15:58:49 -0500

    Somehow Main.java didn't get included in that last commit, whoops

---
+++ src/main/java/com/sciaps/Main.java
@@ -0,0 +1,41 @@
+package com.sciaps;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+/**
+ *
+ * @author sgowen
+ */
+public final class Main
+{
+    public static void main(String[] args)
+    {
+        JFrame.setDefaultLookAndFeelDecorated(true);
+
+        SwingUtilities.invokeLater(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                try
+                {
+                    UIManager.setLookAndFeel("org.pushingpixels.substance.api.skin.SubstanceGraphiteLookAndFeel");
+                }
+                catch (Exception e)
+                {
+                    System.err.println("Substance Graphite failed to initialize");
+                    Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, e);
+                }
+
+                MainFrame mainFrame = new MainFrame();
+                mainFrame.setVisible(true);
+            }
+        });
+    }
+}

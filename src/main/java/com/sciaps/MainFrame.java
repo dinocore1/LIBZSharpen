c629cc3a7a765ddc109cb716be325de0f6136a1f
Stephen Gowen <sgowen@technegames.com>
2014-10-31 14:21:28 -0500

    Forgot to include this in the last commit

---
+++ src/main/java/com/sciaps/MainFrame.java
@@ -0,0 +1,50 @@
+package com.sciaps;
+
+import com.sciaps.view.LIBZUnitConnectedPanel;
+import com.sciaps.view.LIBZUnitDisconnectedPanel;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author sgowen
+ */
+public final class MainFrame extends JFrame
+{
+    public MainFrame()
+    {
+        super("LIBZ Sharpen");
+
+        initUIForOnLIBZUnitDisconnected();
+
+        setSize(640, 480);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+    }
+
+    public void onLIBZUnitConnected()
+    {
+        getContentPane().removeAll();
+        add(new LIBZUnitConnectedPanel(this));
+        refreshUI();
+    }
+
+    public void refreshUI()
+    {
+        revalidate();
+        repaint();
+    }
+
+    public void onLIBZUnitDisconnected()
+    {
+        getContentPane().removeAll();
+        setJMenuBar(null);
+        initUIForOnLIBZUnitDisconnected();
+        refreshUI();
+    }
+
+    private void initUIForOnLIBZUnitDisconnected()
+    {
+        add(new LIBZUnitDisconnectedPanel(this));
+    }
+}

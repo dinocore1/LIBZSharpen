9517470e8be5fa6213fbdc737735a100810eb91f
Stephen Gowen <sgowen@technegames.com>
2014-12-01 14:28:06 -0600

    Forgot to commit RegionsJXCollapsiblePane

---
+++ src/main/java/com/sciaps/view/tabs/defineregions/RegionsJXCollapsiblePane.java
@@ -0,0 +1,85 @@
+package com.sciaps.view.tabs.defineregions;
+
+import com.sciaps.common.data.EmissionLine;
+import com.sciaps.common.data.Region;
+import com.sciaps.global.LibzUnitManager;
+import com.sciaps.view.tabs.common.RegionsPanel;
+import com.sciaps.view.tabs.common.RegionsPanel.RegionsPanelCallback;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.BorderFactory;
+import org.apache.commons.lang.math.DoubleRange;
+import org.jdesktop.swingx.JXCollapsiblePane;
+import org.jfree.chart.plot.Marker;
+
+/**
+ *
+ * @author sgowen
+ */
+public final class RegionsJXCollapsiblePane extends JXCollapsiblePane
+{
+    public interface RegionsJXCollapsiblePaneCallback
+    {
+        void removeChartMarkers(Marker[] regionMarkers);
+    }
+
+    private final Map<String, Marker[]> regionAndAssociatedMarkersMap = new HashMap<String, Marker[]>();
+    private final RegionsJXCollapsiblePaneCallback _callback;
+    private final RegionsPanel _regionsPanel;
+
+    public RegionsJXCollapsiblePane(Direction direction, RegionsJXCollapsiblePaneCallback callback)
+    {
+        super(direction);
+
+        _callback = callback;
+
+        getContentPane().setLayout(new javax.swing.BoxLayout(getContentPane(), javax.swing.BoxLayout.Y_AXIS));
+
+        _regionsPanel = new RegionsPanel(new RegionsPanelCallback()
+        {
+            @Override
+            public void onRegionDeleted(String regionName)
+            {
+                Marker[] markersAssociatedWithRegion = regionAndAssociatedMarkersMap.get(regionName);
+
+                if (markersAssociatedWithRegion != null)
+                {
+                    _callback.removeChartMarkers(markersAssociatedWithRegion);
+                }
+            }
+        });
+        _regionsPanel.setBorder(BorderFactory.createEmptyBorder(15, 0, 0, 0));
+
+        add(_regionsPanel);
+    }
+
+    public void refresh()
+    {
+        _regionsPanel.refresh();
+    }
+
+    public void addRegion(String regionName, int wavelengthMin, int wavelengthMax, Marker... associatedMarkers)
+    {
+        try
+        {
+            Region region = new Region();
+            region.wavelengthRange = new DoubleRange(wavelengthMin, wavelengthMax);
+            region.name = EmissionLine.parse(regionName);
+
+            LibzUnitManager.getInstance().getRegions().add(region);
+
+            Marker[] markers = new Marker[associatedMarkers.length];
+            System.arraycopy(associatedMarkers, 0, markers, 0, associatedMarkers.length);
+
+            regionAndAssociatedMarkersMap.put(region.name.name, markers);
+
+            refresh();
+        }
+        catch (Exception ex)
+        {
+            Logger.getLogger(RegionsJXCollapsiblePane.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}

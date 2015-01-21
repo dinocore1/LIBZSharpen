7b4332445431be60248828daac01ef57e2edc775
Stephen Gowen <sgowen@technegames.com>
2015-01-21 09:36:00 -0600

    Oops, didn't mean to commit MockedLibzUnitApiHandler

--- src/main/java/com/sciaps/Main.java
+++ src/main/java/com/sciaps/Main.java
@@ -2,7 +2,6 @@ package com.sciaps;
 
 import com.sciaps.common.swing.global.InstanceManager;
 import com.sciaps.common.swing.libzunitapi.HttpLibzUnitApiHandler;
-import com.sciaps.common.swing.libzunitapi.MockedLibzUnitApiHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -45,6 +44,6 @@ public final class Main
 
     private static void initModules()
     {
-        InstanceManager.getInstance().storeInstance(HttpLibzUnitApiHandler.class, new MockedLibzUnitApiHandler());
+        InstanceManager.getInstance().storeInstance(HttpLibzUnitApiHandler.class, new HttpLibzUnitApiHandler());
     }
 }

d0cd6202edfe839ddab169a24fe2d06fb9327e31
Paul Soucy <paul@dev-smart.com>
2015-01-27 15:12:16 -0500

    yea got layout working like i want

--- src/main/java/com/sciaps/components/IRBox.java
+++ src/main/java/com/sciaps/components/IRBox.java
@@ -8,6 +8,8 @@ import org.apache.batik.transcoder.TranscoderException;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -24,33 +26,29 @@ public class IRBox extends JComponent {
 
         public RegionBox(Region r) {
             super();
-            setBackground(Color.BLUE);
             mRegion = r;
-            setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
-            //setLayout(new MigLayout("debug"));
-            //setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 1));
+            //setBackground(Color.BLUE);
 
+            Box hbox = Box.createHorizontalBox();
+            add(hbox);
 
             mLabel = new JLabel(r.name);
-            mLabel.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
+            mLabel.setBorder(BorderFactory.createEmptyBorder(2, 2, 2, 2));
             mLabel.setFont(mLabel.getFont().deriveFont(Font.BOLD, 18));
-            add(mLabel);
-            //add(mLabel, "span 2 2");
+            hbox.add(mLabel);
 
             Box vbox = Box.createVerticalBox();
+            hbox.add(vbox);
 
             mDeleteButton = createIconButton(getClass().getResource("/icons/svg/delete104.svg"), 15, 15);
             vbox.add(mDeleteButton);
 
-            vbox.add(Box.createVerticalStrut(20));
-            //add(mDeleteButton, "cell 3 0");
+            vbox.add(Box.createVerticalStrut(10));
 
             mEditButton = createIconButton(getClass().getResource("/icons/svg/settings60.svg"), 15, 15);
             vbox.add(mEditButton);
-            //add(mEditButton, "cell 3 1");
 
-            add(vbox);
-
+            setMaximumSize(getPreferredSize());
 
         }
 
@@ -63,23 +61,47 @@ public class IRBox extends JComponent {
             } catch (TranscoderException e) {
                 e.printStackTrace();
             }
+            retval.setOpaque(false);
+            retval.setContentAreaFilled(false);
             retval.setIcon(icon);
             return retval;
         }
+
     }
 
 
     private IRRatio mIRatio;
 
+    private JScrollPane mNumScroll;
+    private JScrollPane mDenomScroll;
+
     private final Box mNumBoxLayout;
     private final Box mDemBoxLayout;
 
+    private final JButton mNumAddButton;
+    private final JButton mDemAddButton;
+
     public IRBox() {
         mNumBoxLayout = Box.createHorizontalBox();
-        add(mNumBoxLayout);
+        JScrollPane scrollPane = new JScrollPane(mNumBoxLayout);
+        mNumScroll = scrollPane;
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_NEVER);
+        scrollPane.setBorder(BorderFactory.createEmptyBorder());
+        add(scrollPane);
 
         mDemBoxLayout = Box.createHorizontalBox();
-        add(mDemBoxLayout);
+        scrollPane = new JScrollPane(mDemBoxLayout);
+        mDenomScroll = scrollPane;
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_NEVER);
+        scrollPane.setBorder(BorderFactory.createEmptyBorder());
+        add(scrollPane);
+
+        mNumAddButton = createAddNewButton();
+        mDemAddButton = createAddNewButton();
+
+
     }
 
     public void setIRRatio(IRRatio irRatio) {
@@ -87,7 +109,6 @@ public class IRBox extends JComponent {
 
         mNumBoxLayout.removeAll();
         mDemBoxLayout.removeAll();
-
 
         mNumBoxLayout.add(Box.createHorizontalGlue());
         for(Iterator<Region> it = irRatio.numerator.iterator();it.hasNext();) {
@@ -99,6 +120,9 @@ public class IRBox extends JComponent {
                 mNumBoxLayout.add(createPlusSymbol());
             }
         }
+
+        mNumBoxLayout.add(Box.createHorizontalStrut(20));
+        mNumBoxLayout.add(mNumAddButton);
         mNumBoxLayout.add(Box.createHorizontalGlue());
 
         mDemBoxLayout.add(Box.createHorizontalGlue());
@@ -109,9 +133,10 @@ public class IRBox extends JComponent {
 
             if(it.hasNext()){
                 mDemBoxLayout.add(createPlusSymbol());
-
             }
         }
+        mDemBoxLayout.add(Box.createHorizontalStrut(20));
+        mDemBoxLayout.add(mDemAddButton);
         mDemBoxLayout.add(Box.createHorizontalGlue());
     }
 
@@ -126,6 +151,25 @@ public class IRBox extends JComponent {
         }
     }
 
+    private JButton createAddNewButton() {
+        try {
+            SVGIcon icon = new SVGIcon(getClass().getResource("/icons/svg/add201.svg").toString(), 20, 20);
+            JButton retval = new JButton(icon);
+            //retval.setMaximumSize(new Dimension(20, 20));
+            //retval.setMinimumSize(new Dimension(20, 20));
+            //retval.setBorder(BorderFactory.createEmptyBorder(2, 2, 2, 2));
+            //retval.setOpaque(false);
+            retval.setContentAreaFilled(false);
+
+
+
+            return retval;
+
+        } catch (TranscoderException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 
     @Override
     public Dimension getMinimumSize() {
@@ -150,6 +194,9 @@ public class IRBox extends JComponent {
         return perferedSize;
     }
 
+
+    final int lineHeight = 5;
+
     @Override
     public void doLayout() {
         final Container parent = getParent();
@@ -157,25 +204,24 @@ public class IRBox extends JComponent {
         int maxWidth = parent.getWidth() - (insets.left + insets.right);
         int maxHeight = parent.getHeight() - (insets.top + insets.bottom);
 
-        mNumBoxLayout.setBounds(0, 0, maxWidth, (maxHeight - 20) / 2);
-
-        mDemBoxLayout.setBounds(0, (maxHeight + 20)/2, maxWidth, (maxHeight - 20) / 2);
+        mNumScroll.setBounds(0, 0, maxWidth, (maxHeight - lineHeight) / 2  - 10);
+        mDenomScroll.setBounds(0, (maxHeight + lineHeight)/2 + 10, maxWidth, (maxHeight - lineHeight) / 2 - 10);
     }
 
 
     @Override
     protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
+        Graphics tmp = g.create();
 
         final int width = getWidth();
         final int height = getHeight();
 
-        final int lineHeight = 15;
-
-        Graphics2D g2 = (Graphics2D) g;
+        Graphics2D g2 = (Graphics2D) tmp;
         g2.setColor(Color.BLACK);
 
         g2.fillRect(0, (height - lineHeight) / 2, width, lineHeight);
+
+        super.paintComponent(g);
 
 
     }
